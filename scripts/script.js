0bad7ee3c96279a7eb82a51001c911b1236bf667
Tony Eder <tonyeder11@gmail.com>
2023-09-28 06:54:26 -0700

    ongoing style changes to fonts, images, etc

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by Javascript";
+// document.querySelector("body").appendChild(h2);
 
 const haters = ["No worries, I get it.","Okay, okay, okay. 2x is fine.","Wow, really?? A third click now?","Beginning to think you are just toying around here.","You have reached your max for clicks on this button!!", "This message will self-destruct in 5, 4, 3, 2, 1....1....1....5, 4..."];
 
