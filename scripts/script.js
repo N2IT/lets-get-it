0a85252c6f5022f4fb99b314fa47b1150aba105a
N2IT <32651728+N2IT@users.noreply.github.com>
2023-11-27 10:57:42 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,11 @@
 // h2.textContent = "This content added by Javascript";
 // document.querySelector("body").appendChild(h2);
 
-const haters = ["No worries, I get it.","Okay, okay, okay. 2x is fine.","Wow, really?? A third click now?","Beginning to think you are just toying around here.","You have reached your max for clicks on this button!!", "This message will self-destruct in 5, 4, 3, 2, 1....1....1....5, 4..."];
 
-    function dontMessage(haters) {
-    for (let i = 0; i < haters.length; i++) {
-        alert(`${haters[i]}`);
-    };
-};
+let dontHate = document.querySelector('#dontmessage')
+const haters = ["No worries, I get it.","Okay, okay, okay. 2x is fine.","Wow, really?? A third click now?","Beginning to think you are just toying around here.","You have reached your max for clicks on this button!!", "This message will self-destruct in 5, 4, 3, 2, 1....1....1....5, 4..."]
+let currentIndex = 0
+dontHate.addEventListener('click', () =>{
+    alert(haters[currentIndex])
+    currentIndex = (currentIndex + 1) % haters.length
+})
